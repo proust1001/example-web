e25d8e3dbf99c2701d8c8dad03c030a7287d4fbe
proust1001 <96327174+proust1001@users.noreply.github.com>
2023-01-25 01:53:09 +0700

    coding challenge 3

---
+++ script3.js
@@ -0,0 +1,31 @@
+let averageDolphinsScore = (96 + 108 + 89) / 3;
+let averageKoalasScore = (88 + 91 + 110) / 3;
+if (averageDolphinsScore > averageKoalasScore) {
+    console.log("The winner is team Dolphins.");
+} else if (averageDolphinsScore < averageKoalasScore) {
+    console.log("The winner is team Koalas.");
+} else {
+    console.log("It is a draw.");
+}
+
+averageDolphinsScore = (97 + 112 + 101) / 3;
+averageKoalasScore = (109 + 95 + 123) / 3;
+if (averageDolphinsScore > averageKoalasScore && averageDolphinsScore >= 100) {
+    console.log("The winner is team Dolphins.");
+} else if (averageDolphinsScore < averageKoalasScore && averageKoalasScore >= 100) {
+    console.log("The winner is team Koalas.");
+} else {
+    console.log("It is a draw.");
+}
+
+averageDolphinsScore = (97 + 112 + 101) / 3;
+averageKoalasScore = (109 + 95 + 106) / 3;
+if (averageDolphinsScore > averageKoalasScore && averageDolphinsScore >= 100) {
+    console.log("The winner is team Dolphins.");
+} else if (averageDolphinsScore < averageKoalasScore && averageKoalasScore >= 100) {
+    console.log("The winner is team Koalas.");
+} else if (averageDolphinsScore = averageKoalasScore && averageDolphinsScore + averageKoalasScore >= 200) {
+    console.log("It is a draw.");
+} else {
+    console.log("There is no winner.");
+}
